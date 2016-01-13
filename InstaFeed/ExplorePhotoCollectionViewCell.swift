3c3170526e28667facf166a2a960dd88c7661bda
arybin <andrew.rybin@gmail.com>
2016-01-13 16:23:39 -0500

    working on adding more features

---
+++ InstaFeed/ExplorePhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExplorePhotoCollectionViewCell.swift
+//  InstaFeed
+//
+//  Created by Andrei Rybin on 1/13/16.
+//  Copyright © 2016 Andrei Rybin. All rights reserved.
+//
+
+import UIKit
+
+class ExplorePhotoCollectionViewCell: UICollectionViewCell {
+    
+}
